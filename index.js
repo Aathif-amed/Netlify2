16c713cf8228dbab8acc09529c192413f7a9f25e
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2022-09-01 14:43:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function getDetails() {
+  var fname = document.getElementById("fName").value;
+  var mname = document.getElementById("mName").value;
+  var lname = document.getElementById("lName").value;
+  var email = document.getElementById("email").value;
+  console.log(`${fname}
+  ${mname}
+  ${lname}
+  ${email}`);
+}
